f137e454787d5c2ff2b333996c8021db6f7cd067
yirolhao <yirolhao@gmail.com>
2016-11-05 22:31:43 +0800

    Finsh user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
-Rails.application.routes.draw do
-	
-  get 'users/new'
+Rails.application.routes.draw do	
 
   root 'static_pages#home'
   	
@@ -9,5 +7,12 @@ Rails.application.routes.draw do
   get '/about' ,to: 'static_pages#about'
   
   get '/contact', to: 'static_pages#contact'
-	get '/signup' , to: 'users#new'
+	
+  get '/signup' , to: 'users#new'
+
+  post '/signup', to: 'users#create'
+
+  get '/signup', to: 'users#show'
+
+  resources :users
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test "invalid signup information" do
+	get signup_path
+	assert_difference 'User.count' ,1 do
+		post users_path, params: { user:{ name: "user",email: "user@valid.com",password: "password",
+											password_confirmation: "password"} }
+	end  	
+	#assert_template 'users/new'
+	#assert_select 'div#<CSS id for error explanation>'
+	#assert_select 'div.<CSS class for field with error>'
+	follow_redirect!
+    assert_template 'users/show'
+  end
+end
